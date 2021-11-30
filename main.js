53c05f5ffe1e82d8cef59c1b769fa8e4d61e3671
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2021-11-30 19:28:45 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+// abre e fecha o menu quando clicamos no hamburger ou no X
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+// quando clicar em um item do menu fecha-
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+// mudar o header da pagina quando der scroll
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+    
+    if (window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
+}
+
+
+// Testimonials carousel slider swiper
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard:  true,
+    breakpoints: {
+        767:{
+            slidesPerView: 2,
+            sertWrapperSize: true
+        }
+    }
+
+})
+
+// ScrollReveal: Mostrar elesmentos quando der scroll na página
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '300px',
+    duration: 300,
+    reset: true 
+})
+scrollReveal.reveal(`#home .text, #home .image, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, footer .brand, footer .social`, {interval: 200})
+
+// Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+    if (window.scrollY >= 700) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+const sections = document.querySelectorAll('section[id]')
+function activateMenuAtCurrentSection() {
+    
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for( const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.add('active')
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']' )
+            .classList.remove('active')
+        }
+    }
+}
+
+// quando scrolla
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
